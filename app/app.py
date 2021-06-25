3072e95a7245724d5f8c972fe8266770b6a754b5
Gaurav Reddy <gauravreddy008@gmail.com>
2021-06-26 01:50:52 +0530

    Updated app.py

--- app/app.py
+++ app/app.py
@@ -24,12 +24,13 @@ st.set_page_config(page_title="WFD : beta")
 #### Main Body ####
 
 
-st.title("Detecting Wild Fires 🌳🔥 `Beta`")
-st.write("To know more about this app, visit [**GitHub**](https://github.com/gauravreddy08)")
+st.title("Xtinguish `Beta`")
+st.write("**Xtinguish** is an CNN Image Classfication model which helps in detecting and preventing **Wildfires**.")
+st.write("To know more about this app, visit [**GitHub**](https://github.com/gauravreddy08/Xtinguish)")
 file = st.file_uploader(label="Upload an image",
                         type=["jpg", "jpeg", "png"])
 
-model = tf.keras.models.load_model("../models/baseline_model.hdf5")
+model = tf.keras.models.load_model("./models/baseline_model.hdf5")
 
 if not file:
     st.warning("Please upload an image")
